74e21dd9c7833e65ceb065aa485ae44ebd9fec69
GOLDER303 <szymonstepien@interia.pl>
2023-06-17 19:18:18 +0200

    Change the way input is handled

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
 
-    private InputActions inputActions;
     private Vector2 moveVector = Vector2.zero;
     private Vector2 mousePosition = Vector2.zero;
     private Rigidbody2D playerRigidBody;
 
     private void Awake()
     {
-        inputActions = new InputActions();
         playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -34,34 +32,13 @@ public class PlayerMovement : MonoBehaviour
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 
-    private void OnEnable()
+    private void OnMove(InputValue inputValue)
     {
-        inputActions.Enable();
-        inputActions.Player.Move.performed += OnMovementPerformed;
-        inputActions.Player.Move.canceled += OnMovementCanceled;
-        inputActions.Player.PointerPosition.performed += OnPointerPositionPerformed;
+        moveVector = inputValue.Get<Vector2>();
     }
 
-    private void OnDisable()
+    private void OnPointerPosition(InputValue inputValue)
     {
-        inputActions.Disable();
-        inputActions.Player.Move.performed -= OnMovementPerformed;
-        inputActions.Player.Move.canceled -= OnMovementCanceled;
-        inputActions.Player.PointerPosition.performed -= OnPointerPositionPerformed;
-    }
-
-    private void OnMovementPerformed(InputAction.CallbackContext callbackContext)
-    {
-        moveVector = callbackContext.ReadValue<Vector2>();
-    }
-
-    private void OnMovementCanceled(InputAction.CallbackContext callbackContext)
-    {
-        moveVector = Vector2.zero;
-    }
-
-    private void OnPointerPositionPerformed(InputAction.CallbackContext callbackContext)
-    {
-        mousePosition = callbackContext.ReadValue<Vector2>();
+        mousePosition = inputValue.Get<Vector2>();
     }
 }
