5163c0dfef416a129162ae237d41863e69dc18b6
GOLDER303 <szymonstepien@interia.pl>
2023-06-17 20:38:52 +0200

    Add player shooting

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float projectileForce = 20f;
+
+    private void OnFire()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D projectileRigidBody = projectile.GetComponent<Rigidbody2D>();
+        projectileRigidBody.AddForce(firePoint.up * projectileForce, ForceMode2D.Impulse);
+        Destroy(projectile, 4f);
+    }
+}
