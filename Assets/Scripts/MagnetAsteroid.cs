6d36feed49f9f73edd50da1ae1e2c83328621b57
GOLDER303 <szymonstepien@interia.pl>
2023-06-18 20:15:54 +0200

    Spawn magnet pebbles after magnet asteroid is destroyed

--- Assets/Scripts/MagnetAsteroid.cs
+++ Assets/Scripts/MagnetAsteroid.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class MagnetAsteroid : Asteroid
 {
+    [SerializeField] private GameObject magnetPebble;
+    [SerializeField] private int magnetPebbleAmount = 5;
+
     protected override void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Projectile"))
         {
             Destroy(gameObject);
+
+            for (int i = 0; i < magnetPebbleAmount; i++)
+            {
+                SpawnMagnetPebble();
+            }
         }
+    }
+
+    private void SpawnMagnetPebble()
+    {
+        Vector3 spawnPosition = Random.insideUnitCircle;
+        spawnPosition += transform.position;
+        Instantiate(magnetPebble, spawnPosition, Quaternion.identity);
     }
 }
