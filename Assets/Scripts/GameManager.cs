d08e4787fd49c9796f3f4420ed056ebc37a07b09
GOLDER303 <szymonstepien@interia.pl>
2023-06-19 19:10:13 +0200

    Zoom out the camera

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameOverScreen gameOverScreen;
     [SerializeField] LeaderboardManager leaderboardManager;
+    [SerializeField] TextMeshProUGUI inGameScoreText;
 
     private int score;
 
     public void GameOver()
     {
+        inGameScoreText.enabled = false;
         gameOverScreen.Setup(score);
-        StartCoroutine(leaderboardManager.SubmitScoreRoutine(score));
-        StartCoroutine(leaderboardManager.FetchTopHighScoresRoutine());
+        StartCoroutine(SubmitScore(score));
+    }
+
+    private IEnumerator SubmitScore(int score)
+    {
+        yield return leaderboardManager.SubmitScoreRoutine(score);
+        yield return leaderboardManager.FetchTopHighScoresRoutine();
     }
 
     public void AddScore(int scoreToAdd)
     {
         score += scoreToAdd;
+        inGameScoreText.text = "Score: " + score;
     }
 }
